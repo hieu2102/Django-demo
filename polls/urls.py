bb4825f8d3cfd1f04f4fa7c70f88497acafcc6f9
hieu2102 <hieund2102@gmail.com>
2019-08-27 01:34:07 +0700

    fill content

--- polls/urls.py
+++ polls/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('details/<int:question_id>',views.detail,name = 'detail'),
     path('details/<int:question_id>/result/', views.results,name = 'results'),
     path('details/<int:question_id>/vote/', views.vote, name = 'vote'),
+
 ]
