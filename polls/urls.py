b21c89455b08adbfafc9a1e91b2a6b46bdb733bb
hieu2102 <hieund2102@gmail.com>
2019-08-27 01:54:39 +0700

    replace redundant fuctions in view with framework generics

--- polls/urls.py
+++ polls/urls.py
@@ -6,9 +6,13 @@ app_name = 'polls'
 
 #url list
 urlpatterns = [
-    path('',views.index, name = 'index'),
-    path('details/<int:question_id>',views.detail,name = 'detail'),
-    path('details/<int:question_id>/result/', views.results,name = 'results'),
+    #path('',views.index, name = 'index'),
+    #path('details/<int:question_id>',views.detail,name = 'detail'),
+    #path('details/<int:question_id>/result/', views.results,name = 'results'),
+    #path('details/<int:question_id>/vote/', views.vote, name = 'vote'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('details/<int:pk>', views.DetailView.as_view(), name='details'),
+    path('details/<int:pk>/results', views.ResultsView.as_view(), name= 'results'),
     path('details/<int:question_id>/vote/', views.vote, name = 'vote'),
 
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,31 +2,32 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404,HttpResponseRedirect
 from django.template import loader
 from django.utils import timezone
+from django.views import generic
 from django.urls import reverse
 from .models import Question,Choice
 # Create your views here.
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     # template = loader.get_template('polls/index.html')
     # context = {
     #     'latest_question_list':latest_question_list,
     # }
     # return HttpResponse(template.render(context, request))
-    context = {
-        'latest_question_list':latest_question_list,
-    }
-    return render(request,'polls/index.html',context)
+    # context = {
+    #     'latest_question_list':latest_question_list,
+    # }
+    # return render(request,'polls/index.html',context)
 
-def detail(request, question_id):
-    question =get_object_or_404(Question,pk=question_id)
-    return render(request, 'polls/details.html', {'question':question})
-
-def results(request,question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request,'polls/results.html',
-                  {'question':question,})
+# def detail(request, question_id):
+#     question =get_object_or_404(Question,pk=question_id)
+#     return render(request, 'polls/details.html', {'question':question})
+#
+# def results(request,question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request,'polls/results.html',
+#                   {'question':question,})
 
 def vote(request,question_id):
     question = get_object_or_404(Question,pk=question_id)
@@ -42,3 +43,18 @@ def vote(request,question_id):
         selected_choice.votes +=1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results',args=(question_id,)))
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    def get_queryset(self):
+        """return last 05 published questions"""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/details.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
